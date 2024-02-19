1b044201f01e807dcc029c26d13e8fc390c8822a
Pablo Molina <molinapablod@gmail.com>
2024-02-20 09:36:54 +1100

    animated transitions

--- app/src/main/java/com/pdm/to_do_compose/presentation/navigation/SetupNavigation.kt
+++ app/src/main/java/com/pdm/to_do_compose/presentation/navigation/SetupNavigation.kt
@@ -1,6 +1,8 @@
 package com.pdm.to_do_compose.presentation.navigation
 
 import android.util.Log
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
@@ -24,9 +26,23 @@ fun SetupNavigation(
     }
 
     NavHost(navController = navController, startDestination = LIST_SCREEN) {
-        composable(LIST_SCREEN, arguments = listOf(navArgument(LIST_ARG_KEY) {
-            type = NavType.StringType
-        })) {
+        composable(
+            LIST_SCREEN, arguments = listOf(navArgument(LIST_ARG_KEY) {
+                type = NavType.StringType
+            }),
+            enterTransition = {
+                scaleIntoContainer()
+            },
+            exitTransition = {
+                scaleOutOfContainer(direction = ScaleTransitionDirection.INWARDS)
+            },
+            popEnterTransition = {
+                scaleIntoContainer(direction = ScaleTransitionDirection.OUTWARDS)
+            },
+            popExitTransition = {
+                scaleOutOfContainer()
+            }
+        ) {
             ToDoListScreen {
                 screen.task(it)
                 Log.d("TaskId", it.toString())
@@ -35,7 +51,32 @@ fun SetupNavigation(
 
         composable(TASK_SCREEN, arguments = listOf(navArgument(TASK_ARG_KEY) {
             type = NavType.IntType
-        })) {
+        }),
+            enterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            popEnterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Right,
+                    animationSpec = tween(700)
+                )
+            },
+            popExitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Right,
+                    animationSpec = tween(700)
+                )
+            }
+        ) {
             TaskScreen {
                 screen.list(it)
             }
